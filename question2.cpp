034592c7a5dd8883ef059bf4ff2bfa30ac18d469
UbuntuPVC <pvcubuntu@gmail.com>
2024-03-18 13:55:47 +0530

    Done with W1A1

---
+++ question2.cpp
@@ -0,0 +1,12 @@
+//Write a Program to print 10 from 2 digits, smaller than 6.
+
+#include <iostream>
+using namespace std;
+
+int main(void) {
+    int x = 5;
+    int y = x;
+    int z = x + y;
+
+    cout << z << "\n";
+}
---
+++ question5.cpp
@@ -0,0 +1,12 @@
+// Write a program to find the circumference of a circle with radius 10
+
+#include <iostream>
+using namespace std;
+
+int main(void) {
+    int radius = 10;
+
+    float circumference = 2 * 3.14 * radius;
+
+    cout << circumference << "\n"; 
+}
